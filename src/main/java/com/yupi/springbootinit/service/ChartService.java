5d0911d97feda65e61891412df895086e7f6be6d
becoze <liyuanliang20@outlook.com>
2023-12-13 21:45:42 +1100

    Initial commit

---
+++ src/main/java/com/yupi/springbootinit/service/ChartService.java
@@ -0,0 +1,13 @@
+package com.yupi.springbootinit.service;
+
+import com.yupi.springbootinit.model.entity.Chart;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author liyua
+* @description 针对表【chart(Chart)】的数据库操作Service
+* @createDate 2023-09-22 00:30:09
+*/
+public interface ChartService extends IService<Chart> {
+
+}
