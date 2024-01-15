36f4872a0c02796303d6a062fff3738e2d2079fa
becoze <liyuanliang20@outlook.com>
2024-01-16 02:26:01 +1100

    add Redis features, create redis config object

--- src/main/java/com/becoze/biback/config/RedissonConfig.java
+++ src/main/java/com/becoze/biback/config/RedissonConfig.java
@@ -12,14 +12,20 @@ import org.springframework.context.annotation.Configuration;
 @ConfigurationProperties(prefix = "spring.redis")
 @Data
 public class RedissonConfig {
+    private Integer database;
+
+    private String host;
+
+    private Integer port;
+
+    private String password;
     @Bean
-    public RedissonClient getRedisson(){
+    public RedissonClient redissonClient(){
         // Create config object
         Config config = new Config();
         config.useSingleServer()
-                .setDatabase(1)
-                .setAddress("redis://127.0.0.1:6379")
-                .setPassword("123456");
+                .setDatabase(database)
+                .setAddress("redis://" + host + ":" + port);
 
         // Sync and Async API
         RedissonClient redisson = Redisson.create(config);
