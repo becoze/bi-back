f66351c62aed7e200fd8b2d79a280f9f6e7b40d3
becoze <liyuanliang20@outlook.com>
2024-01-18 17:41:45 +1100

    feat add queue-controller for test

---
+++ src/main/java/com/becoze/biback/controller/QueueController.java
@@ -0,0 +1,64 @@
+package com.becoze.biback.controller;
+
+import cn.hutool.json.JSONUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * Queue test controller
+ */
+@RestController
+@RequestMapping("/queue")
+@Slf4j
+public class QueueController {
+
+    @Resource
+    private ThreadPoolExecutor threadPoolExecutor;
+
+
+    /**
+     * submit a task to the thread pool for texting
+     * @param task
+     */
+    @GetMapping("/add")
+    public void add(String task){
+        CompletableFuture.runAsync(() -> {
+            System.out.println("Progressing task: " + task + " by " + Thread.currentThread().getName());
+
+            try {
+                // the task will remain 60 min
+                Thread.sleep(600000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, threadPoolExecutor);
+    }
+
+    @GetMapping("/get")
+    public String get(){
+        Map<String, Object> map = new HashMap<>();
+
+        int size = threadPoolExecutor.getQueue().size();
+        map.put("Size of the Queue", size);
+
+        long taskCount = threadPoolExecutor.getTaskCount();
+        map.put("# of Tasks", taskCount);
+
+        long completeTaskCount =threadPoolExecutor.getCompletedTaskCount();
+        map.put("# of Completed tasks", completeTaskCount);
+
+        int activeCount = threadPoolExecutor.getActiveCount();
+        map.put("# of Active threads", activeCount);
+
+        return JSONUtil.toJsonStr(map);
+    }
+
+}
