cefc69e5f0ee47fffa598c7078cc87959612d39a
becoze <liyuanliang20@outlook.com>
2024-02-06 22:55:34 +1100

    chore add comments for MQ send

--- src/main/java/com/becoze/biback/mq/SendSingle.java
+++ src/main/java/com/becoze/biback/mq/SendSingle.java
@@ -11,11 +11,21 @@ public class SendSingle {
     private final static String QUEUE_NAME = "hello";
 
     public static void main(String[] argv) throws Exception {
+        // Create connection factory
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
+        // Set username, password, and port if you have server
+//        factory.setUsername();
+//        factory.setPassword();
+//        factory.setPort();
+
+        // Establish connection with server or localhost
+        // Create channel. Channel is the client enable us to operate the Message Queue
         try (Connection connection = factory.newConnection();
              Channel channel = connection.createChannel()) {
+            // Create Message Queue
             channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            // Send message
             String message = "Hello World!";
             channel.basicPublish("", QUEUE_NAME, null, message.getBytes(StandardCharsets.UTF_8));
             System.out.println(" [x] Sent '" + message + "'");
