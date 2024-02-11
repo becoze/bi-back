8131ba8236dc616512477d759f31aaa92fe060f0
becoze <liyuanliang20@outlook.com>
2024-02-11 23:26:58 +1100

    feat implement MQ consumer

---
+++ src/main/java/com/becoze/biback/mq/RecvMuti.java
@@ -0,0 +1,60 @@
+package com.becoze.biback.mq;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+public class RecvMuti {
+
+    private static final String TASK_QUEUE_NAME = "multi_queue";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        final Connection connection = factory.newConnection();
+
+        // loop twist mean 2 consumer
+        for (int i = 0; i < 2; i++) {
+            final Channel channel = connection.createChannel();
+
+            channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
+            System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+            channel.basicQos(1);
+
+            // Define what to do with the message
+            int finalI = i;
+            DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+                String message = new String(delivery.getBody(), "UTF-8");
+
+                try {
+                    // doWork
+                    System.out.println(" [x] Received by Consumer " + finalI + ", '" + message + "'");
+                    Thread.sleep(10000);
+                    channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    channel.basicNack(delivery.getEnvelope().getDeliveryTag(), false, false);
+                } finally {
+                    System.out.println(" [x] Done");
+                    channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+                }
+            };
+            // Enable listing, Consumer
+            channel.basicConsume(TASK_QUEUE_NAME, false, deliverCallback, consumerTag -> {
+            });
+        }
+    }
+
+    private static void doWork(String task) {
+        for (char ch : task.toCharArray()) {
+            if (ch == '.') {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException _ignored) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+    }
+}
