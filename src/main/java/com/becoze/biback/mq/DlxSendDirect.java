85bccfb0bc19baa8e7b6922d5e562b0c41ce6412
becoze <liyuanliang20@outlook.com>
2024-02-23 01:13:06 +1100

    test producer redirect message to dlx queue

--- src/main/java/com/becoze/biback/mq/DlxSendDirect.java
+++ src/main/java/com/becoze/biback/mq/DlxSendDirect.java
@@ -6,16 +6,30 @@ import com.rabbitmq.client.ConnectionFactory;
 
 import java.util.Scanner;
 
-public class SendDirect {
+public class DlxSendDirect {
 
-  private static final String EXCHANGE_NAME = "direct_exchange";
+  private static final String DEAD_EXCHANGE_NAME = "dlx_direct_exchange";
+  private static final String WORK_EXCHANGE_NAME = "work_direct_exchange";
 
   public static void main(String[] argv) throws Exception {
     ConnectionFactory factory = new ConnectionFactory();
     factory.setHost("localhost");
     try (Connection connection = factory.newConnection();
          Channel channel = connection.createChannel()) {
-        channel.exchangeDeclare(EXCHANGE_NAME, "direct");
+
+        // declare DLX exchange
+        channel.exchangeDeclare(DEAD_EXCHANGE_NAME, "direct");
+
+        // declare DLX Message queue "boss", and bind it to the exchange
+        String queue1 = "boss_dlx_queue";
+        channel.queueDeclare(queue1, true, false, false, null);
+        channel.queueBind(queue1, DEAD_EXCHANGE_NAME, "boss");
+
+        // declare DLX Message queue "employee", and bind it to the exchange
+        String queue2 = "third_party_dlx_queue";
+        channel.queueDeclare(queue2, true, false, false, null);
+        channel.queueBind(queue2, DEAD_EXCHANGE_NAME, "third_party");
+
 
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNext()) {
@@ -28,7 +42,7 @@ public class SendDirect {
             String message = strings[0];
             String routingKey = strings[1];
 
-            channel.basicPublish(EXCHANGE_NAME, routingKey, null, message.getBytes("UTF-8"));
+            channel.basicPublish(WORK_EXCHANGE_NAME, routingKey, null, message.getBytes("UTF-8"));
             System.out.println(" [x] Sent '" + message + "' with routing: " + routingKey);
         }
     }
