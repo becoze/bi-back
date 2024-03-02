78c3eb80b766bab4ea876ff8f46390fac7436cb2
becoze <liyuanliang20@outlook.com>
2024-03-03 00:13:43 +1100

    feat init message consumer

--- src/main/java/com/becoze/biback/bizmq/MyMessageConsumer.java
+++ src/main/java/com/becoze/biback/bizmq/MyMessageConsumer.java
@@ -1,17 +1,27 @@
 package com.becoze.biback.bizmq;
 
+import com.rabbitmq.client.Channel;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.AmqpHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 
 @Component
-public class MyMessageProducer {
+@Slf4j
+public class MyMessageConsumer {
 
     @Resource
     private RabbitTemplate rabbitTemplate;
 
-    public void sendMessage(String exchange, String routingKey, String message) {
-        rabbitTemplate.convertAndSend(exchange, routingKey, message);
+    @SneakyThrows
+    @RabbitListener(queues = {}, ackMode = "MANUAL")
+    public void receiveMessage(String message, Channel channel, @Header(AmqpHeaders.DELIVERY_TAG) long deliveryTag) {
+        log.info("receiveMessage message = {}", message);
+        channel.basicAck(deliveryTag, false);
     }
 }
