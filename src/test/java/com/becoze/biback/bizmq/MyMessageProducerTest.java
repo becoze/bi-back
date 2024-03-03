265f8e1df899d5a1dc736d607bfb103aaecfea31
becoze <liyuanliang20@outlook.com>
2024-03-03 22:25:27 +1100

    test mq

--- src/test/java/com/becoze/biback/bizmq/MyMessageProducerTest.java
+++ src/test/java/com/becoze/biback/bizmq/MyMessageProducerTest.java
@@ -1,4 +1,17 @@
+package com.becoze.biback.bizmq;
+
+import org.junit.jupiter.api.Test;
+
+import javax.annotation.Resource;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class MyMessageProducerTest {
-  
+    @Resource
+    private MyMessageProducer myMessageProducer;
+
+    @Test
+    void sendMessage() {
+        myMessageProducer.sendMessage("code_exchange", "my_routingKey", "Hello World!");
+    }
 }
