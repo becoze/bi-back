7fbacd78df7cb93674b325c177c81220888b5ac8
becoze <liyuanliang20@outlook.com>
2024-03-09 18:19:27 +1100

    chore name changing for BiMessageConsumer

--- src/main/java/com/becoze/biback/bizmq/BiMessageConsumer.java
+++ src/main/java/com/becoze/biback/bizmq/BiMessageConsumer.java
@@ -1,23 +1,21 @@
 package com.becoze.biback.bizmq;
 
+import com.becoze.biback.constant.BiMqConstant;
 import com.rabbitmq.client.Channel;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.support.AmqpHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.Resource;
-
 @Component
 @Slf4j
-public class MyMessageConsumer {
+public class BiConsumer {
 
     // queues = {"..."} the name of queue listening to; ackMode = "..." the ack mode: "MANUAL" or "AUTO"
     // same concept as `channelName.basicConsume(...)`, @RabbitListener header automatic do it for us.
 
-    @RabbitListener(queues = {"code_queue"}, ackMode = "MANUAL")
+    @RabbitListener(queues = {BiMqConstant.BI_QUEUE_NAME}, ackMode = "MANUAL")
     public void receiveMessage(String message, Channel channel, @Header(AmqpHeaders.DELIVERY_TAG) long deliveryTag) {
         log.info("receiveMessage message = {}", message);
         try {
