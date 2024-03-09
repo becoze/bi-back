896bbbe0631a6a1ed675ad41dc6f5fb512ffebc2
becoze <liyuanliang20@outlook.com>
2024-03-09 18:18:38 +1100

    chore name changing for BiMessageProducer

---
+++ src/main/java/com/becoze/biback/bizmq/BiMessageProducer.java
@@ -0,0 +1,22 @@
+package com.becoze.biback.bizmq;
+
+import com.becoze.biback.constant.BiMqConstant;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
+@Component
+public class BiProducer {
+
+    @Resource
+    private RabbitTemplate rabbitTemplate;
+
+    /**
+     * Sending message
+     * @param message the message to be sent
+     */
+    public void sendMessage(String message) {
+        rabbitTemplate.convertAndSend(BiMqConstant.BI_EXCHANGE_NAME, BiMqConstant.BI_ROUTING_KEY, message);
+    }
+}
