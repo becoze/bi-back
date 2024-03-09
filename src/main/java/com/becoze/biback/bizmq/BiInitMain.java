c38158b7914107af39e84240e5c23e7d6abd772e
becoze <liyuanliang20@outlook.com>
2024-03-09 17:47:22 +1100

    feat add Bi message queue and exchange init method

---
+++ src/main/java/com/becoze/biback/bizmq/BiInitMain.java
@@ -0,0 +1,31 @@
+package com.becoze.biback.bizmq;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+/**
+ * Create Message queue(s) and Exchange(s) for testing. MUSH run this once before any MQ operation
+ */
+public class MqInitMain {
+    public static void main(String[] args) {
+
+        try {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.setHost("localhost");
+            Connection connection = factory.newConnection();
+            Channel channel = connection.createChannel();
+
+            String EXCHANGE_NAME = "code_exchange";
+            channel.exchangeDeclare(EXCHANGE_NAME, "direct");
+
+            // Message queue 1
+            String queueName = "code_queue";
+            channel.queueDeclare(queueName, true, false, false, null);
+            channel.queueBind(queueName, EXCHANGE_NAME, "my_routingKey");
+
+        } catch (Exception e) {
+
+        }
+    }
+}
