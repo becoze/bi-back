fc31b90acae21e21adb408109c17b7e40b672ece
becoze <liyuanliang20@outlook.com>
2024-03-09 17:43:37 +1100

    fix add SpringBootTest annotation

--- src/test/java/com/becoze/biback/bizmq/MyMessageProducerTest.java
+++ src/test/java/com/becoze/biback/bizmq/MyMessageProducerTest.java
@@ -1,17 +1,19 @@
 package com.becoze.biback.bizmq;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class MyMessageProducerTest {
     @Resource
     private MyMessageProducer myMessageProducer;
 
     @Test
     void sendMessage() {
-        myMessageProducer.sendMessage("code_exchange", "my_routingKey", "Hello World!");
+        myMessageProducer.sendMessage("code_exchange", "my_routingKey", "HelloWorld!");
     }
 }
