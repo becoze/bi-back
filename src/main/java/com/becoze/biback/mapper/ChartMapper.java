3984989db2737f09ed5e959ac9f217442ecf46c3
becoze <liyuanliang20@outlook.com>
2024-06-05 20:32:42 +1000

    fix mapper comments

--- src/main/java/com/becoze/biback/mapper/ChartMapper.java
+++ src/main/java/com/becoze/biback/mapper/ChartMapper.java
@@ -7,8 +7,8 @@ import java.util.List;
 import java.util.Map;
 
 /**
-* @author liyua
-* @description 针对表【chart(Chart)】的数据库操作Mapper
+* @author becoze
+* @description Database operation mapper for the table "Chart(chart)"
 * @createDate 2023-09-22 00:30:09
 * @Entity com.becoze.biback.model.entity.Chart
 */
--- src/main/java/com/becoze/biback/mapper/UserMapper.java
+++ src/main/java/com/becoze/biback/mapper/UserMapper.java
@@ -4,8 +4,8 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.becoze.biback.model.entity.User;
 
 /**
-* @author liyua
-* @description 针对表【user(User)】的数据库操作Mapper
+* @author becoze
+* @description Database operation mapper for the table "User(user)"
 * @createDate 2023-09-22 00:30:09
 * @Entity com.becoze.biback.model.entity.User
 */
